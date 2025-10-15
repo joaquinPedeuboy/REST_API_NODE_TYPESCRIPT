85f8ba42366451c70bb71792b4f36e1154da00ea
Joaquin Pedeuboy <joaquin.pedeuboy@hotmail.com>
2025-10-15 15:27:31 -0300

    Add PUT and Patch

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -55,5 +55,62 @@ export const createProduct = async (req : Request, res : Response)=> {
     //    console.log(error)
     //}
 
+}
 
+export const updateProduct = async (req : Request, res : Response)=> {
+    try {
+        const { id } = req.params
+        const product = await Product.findByPk(id)
+
+        if(!product) {
+            return res.status(404).json({
+                error: 'Producto no encontrado'
+            })
+        }
+        // ACTUALIZAR
+        await product.update(req.body)
+        await product.save()
+        res.json({data: product})
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+export const updateAvailability = async (req : Request, res : Response)=> {
+    try {
+        const { id } = req.params
+        const product = await Product.findByPk(id)
+
+        if(!product) {
+            return res.status(404).json({
+                error: 'Producto no encontrado'
+            })
+        }
+        // MODIFICAR
+        product.availability = !product.dataValues.availability
+        await product.save()
+        
+        res.json({data: product})
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const deleteProduct = async (req : Request, res : Response)=> {
+    try {
+        const { id } = req.params
+        const product = await Product.findByPk(id)
+
+        if(!product) {
+            return res.status(404).json({
+                error: 'Producto no encontrado'
+            })
+        }
+        
+        await product.destroy()
+        res.json({data: 'Producto Eliminado'})
+    } catch (error) {
+        console.log(error)
+    }
 }
--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express'
 import { body, param } from 'express-validator'
-import { createProduct, getProducts, getProductsById } from './handlers/product'
+import { createProduct, deleteProduct, getProducts, getProductsById, updateAvailability, updateProduct } from './handlers/product'
 import { handleInputErrors } from './middleware'
 
 const router = Router()
 
-// Routing
+// ROUTING
+
+//GET
 router.get('/', getProducts)
 router.get('/:id', 
     param('id').isInt().withMessage('ID no valido'),
@@ -13,6 +15,7 @@ router.get('/:id',
     getProductsById
 )
 
+// POST CREACION
 router.post('/', 
 
     // VALIDACION
@@ -27,16 +30,34 @@ router.post('/',
     createProduct
 )
 
-router.put('/', (req, res)=> {
-    res.json('Desde Put')
-})
+// PUT ACTUALIZAR, REEMPLAZA COMPLETAMENTE UN RECUERSO EXISTENTE 
+router.put('/:id', 
+    // VALIDACION
+    param('id').isInt().withMessage('ID no valido'),
+    body('name')
+        .notEmpty().withMessage('El nombre de producto no puede ir vacio'),
 
-router.patch('/', (req, res)=> {
-    res.json('Desde Patch')
-})
+    body('price')
+        .isNumeric().withMessage('Valor no valido')
+        .notEmpty().withMessage('El precio de producto no puede ir vacio')
+        .custom( value => value > 0).withMessage('El precio de producto no puede ser menor que 0'),
+    body('availability')
+        .isBoolean().withMessage('Valor para disponibilidad no valido'),
+    handleInputErrors,
+    updateProduct
+)
 
-router.delete('/', (req, res)=> {
-    res.json('Desde Delete')
-})
+// PATCH MODIFICAR, REALIZAR MODIFICACIONES PARCIALES EN UN RECURSO EXISTENTE, PERMITE REALIZAR CAMBIOS ESPECIFICOS SIN AFECTAR EL RESTO DE LA INFORMACION
+router.patch('/:id', 
+    param('id').isInt().withMessage('ID no valido'),
+    handleInputErrors,
+    updateAvailability
+)
+
+router.delete('/:id', 
+    param('id').isInt().withMessage('ID no valido'),
+    handleInputErrors,
+    deleteProduct
+)
 
 export default router
